9aa99b494cf3843dd8b2d7d8eaa42e5f15a3348e
John Clema <john.clema@gmail.com>
2020-04-11 18:39:54 +0800

    Adding pug to render pages

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug');
   });
+
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
